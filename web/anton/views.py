502dc84e820396795127027bab7641e3ca60e5e7
Tim Flynn <trflynn89@gmail.com>
2015-12-24 21:24:46 -0500

    Cache busting on version changes

--- web/anton/views.py
+++ web/anton/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic import View
@@ -6,4 +8,7 @@ from django.views.generic import View
 class Anton(View):
 
     def get(self, request, *args, **kwargs):
-        return render_to_response('anton.html');
+        return render_to_response('anton.html',
+            {
+                'version': os.environ['CURRENT_VERSION_ID']
+            });
